b8693bfa1a13a36d1256693aa3c2630c4e311002
Catalin <awdawd>
2021-08-28 22:16:25 +0300

    add config PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,23 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), VitePWA()],
+  plugins: [
+    reactRefresh(),
+    VitePWA({
+      includeAssets: [],
+      manifest: {
+        name: "Vite PWA Test",
+        short_name: "VPT",
+        description: "Vite with PWA test",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
